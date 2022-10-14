92363bc37f72f2e7cc99c9816a25eced635f86f9
pardhu9999 <104009257+pardhu9999@users.noreply.github.com>
2022-10-14 10:50:11 +0530

    Update pardha saradhi & rohith password_strength_checker.py

--- pardha saradhi & rohith password_strength_checker.py
+++ pardha saradhi & rohith password_strength_checker.py
@@ -9,3 +9,34 @@ if(len(v)>=8):
         print("The password is weak")
 else:
     print("You have entered an invalid password.")
+    
+    
+    
+    
+    
+    
+    ####################   PASSWORD GENERATOR  #####################
+import random
+
+import string
+
+print('Welcome to Password generator!')
+
+length = int(input('\nEnter the length of password: '))
+
+lower = string.ascii_lowercase
+
+upper = string.ascii_uppercase
+
+num = string.digits
+
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+temp = random.sample(all, length)
+
+password = "".join(temp)
+
+print(password)
+
